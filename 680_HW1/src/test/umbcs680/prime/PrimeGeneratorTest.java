4778ce1e8279a016d108f2e24bc6056f35675577
chandu37421104 <144286107+chandu37421104@users.noreply.github.com>
2024-02-12 19:49:19 -0500

    updated test file

--- 680_HW1/src/test/umbcs680/prime/PrimeGeneratorTest.java
+++ 680_HW1/src/test/umbcs680/prime/PrimeGeneratorTest.java
@@ -69,11 +69,12 @@ public class PrimeGeneratorTest {
 
 
     @Test
-    public void testGeneratePrimes() {
+    public void GeneratePrimesFrom1To100() {
         PrimeGenerator gen = new PrimeGenerator(1, 100);
         gen.generatePrimes();
         LinkedList<Long> primes = gen.getPrimes();
         Long[] expectedPrimes = {2L, 3L, 5L, 7L, 11L, 13L, 17L, 19L, 23L, 29L, 31L, 37L, 41L, 43L, 47L, 53L, 59L, 61L, 67L, 71L, 73L, 79L, 83L, 89L, 97L};
         assertArrayEquals("Primes between 1 and 100", expectedPrimes, primes.toArray(new Long[0]));
     }
+
 }
