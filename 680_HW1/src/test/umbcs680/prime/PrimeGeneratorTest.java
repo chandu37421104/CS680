f2b9ca27aa3a3fcbdfdcef986af260ce8ab9aaa4
chandu37421104 <144286107+chandu37421104@users.noreply.github.com>
2024-02-12 20:04:27 -0500

    testfile

--- 680_HW1/src/test/umbcs680/prime/PrimeGeneratorTest.java
+++ 680_HW1/src/test/umbcs680/prime/PrimeGeneratorTest.java
@@ -1,80 +1,77 @@
 package umbcs680.prime;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.beans.Transient;
 import java.util.LinkedList;
 
 public class PrimeGeneratorTest {
 
-    @Test(expected = RuntimeException.class)
-    public void InvalidRangeFrom25To13() {
-        new PrimeGenerator(25, 13);
+    @Test
+    void InvalidRangeFrom25To13() {
+        assertThrows(RuntimeException.class, () -> new PrimeGenerator(25, 13));
     }
 
     @Test
-    public void Is60Even() {
+    void Is60Even() {
         PrimeGenerator gen = new PrimeGenerator(1, 100);
-        assertTrue("60 should be even", gen.isEven(60));
+        assertTrue(gen.isEven(60), "60 should be even");
     }
 
     @Test
-    public void Is31Even() {
+    void Is31Even() {
         PrimeGenerator gen = new PrimeGenerator(1, 100);
-        assertFalse("31 should not be even", gen.isEven(31));
+        assertFalse(gen.isEven(31), "31 should not be even");
     }
 
     @Test
-    public void Is2Prime() {
+    void Is2Prime() {
         PrimeGenerator gen = new PrimeGenerator(1, 100);
-        assertTrue("2 is prime", gen.isPrime(2));
+        assertTrue(gen.isPrime(2), "2 is prime");
     }
 
     @Test
-    public void Is31Prime() {
+    void Is31Prime() {
         PrimeGenerator gen = new PrimeGenerator(1, 100);
-        assertTrue("31 is prime", gen.isPrime(31));
+        assertTrue(gen.isPrime(31), "31 is prime");
     }
 
     @Test
-    public void Is53Prime() {
+    void Is53Prime() {
         PrimeGenerator gen = new PrimeGenerator(1, 100);
-        assertTrue("53 is prime", gen.isPrime(53));
+        assertTrue(gen.isPrime(53), "53 is prime");
     }
     
     @Test
-    public void Is67Prime() {
+    void Is67Prime() {
         PrimeGenerator gen = new PrimeGenerator(1, 100);
-        assertTrue("67 is prime", gen.isPrime(67));
+        assertTrue(gen.isPrime(67), "67 is prime");
     }
 
     @Test
-    public void Is1Prime() {
+    void Is1Prime() {
         PrimeGenerator gen = new PrimeGenerator(1, 100);
-        assertFalse("1 is not prime", gen.isPrime(1));
+        assertFalse(gen.isPrime(1), "1 is not prime");
     }
 
     @Test
-    public void Is40Prime() {
+    void Is40Prime() {
         PrimeGenerator gen = new PrimeGenerator(1, 100);
-        assertFalse("40 is not prime", gen.isPrime(40));
+        assertFalse(gen.isPrime(40), "40 is not prime");
     }
 
     @Test
-    public void Is99Prime() {
+    void Is99Prime() {
         PrimeGenerator gen = new PrimeGenerator(1, 100);
-        assertFalse("99 is not prime", gen.isPrime(99));
+        assertFalse(gen.isPrime(99), "99 is not prime");
     }
 
-
     @Test
-    public void GeneratePrimesFrom1To100() {
+    void GeneratePrimesFrom1To100() {
         PrimeGenerator gen = new PrimeGenerator(1, 100);
         gen.generatePrimes();
         LinkedList<Long> primes = gen.getPrimes();
         Long[] expectedPrimes = {2L, 3L, 5L, 7L, 11L, 13L, 17L, 19L, 23L, 29L, 31L, 37L, 41L, 43L, 47L, 53L, 59L, 61L, 67L, 71L, 73L, 79L, 83L, 89L, 97L};
-        assertArrayEquals("Primes between 1 and 100", expectedPrimes, primes.toArray(new Long[0]));
+        assertArrayEquals(expectedPrimes, primes.toArray(new Long[0]), "Primes between 1 and 100");
     }
-
 }
