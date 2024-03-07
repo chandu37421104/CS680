c9ddc98ffdca64cc03feb44c7ea81abc6a683154
chandu37421104 <144286107+chandu37421104@users.noreply.github.com>
2024-03-06 21:11:48 -0500

    updated HW_4 and readme file

--- 680_HW4/src/main/umbcs680/statedesign/OlaScooter.java
+++ 680_HW4/src/main/umbcs680/statedesign/OlaScooter.java
@@ -4,9 +4,10 @@ package umbcs680.statedesign;
 interface ScooterState {
     void sideStandRemoved(OlaScooter scooter);
     void sideStandParked(OlaScooter scooter);
-    void startMusic(OlaScooter scooter);
+    void powerButtonPressed(OlaScooter scooter);
+    void musicStarted(OlaScooter scooter);
+    void musicStopped(OlaScooter scooter);
 }
-
 
 class ParkedState implements ScooterState {
     private static final ParkedState instance = new ParkedState();
@@ -19,8 +20,8 @@ class ParkedState implements ScooterState {
 
     @Override
     public void sideStandRemoved(OlaScooter scooter) {
-        System.out.println("HMI lock screen pops up. Ready to ride.");
-        scooter.setState(RideState.getInstance());
+        System.out.println("Press Power Button to ride.");
+        scooter.setState(StandbyState.getInstance());
     }
 
     @Override
@@ -29,12 +30,57 @@ class ParkedState implements ScooterState {
     }
 
     @Override
-    public void startMusic(OlaScooter scooter) {
-        System.out.println("Music starts playing. Colored lights blinking.");
+    public void powerButtonPressed(OlaScooter scooter) {
+        System.out.println("Please remove the side stand to ride.");
+    }
+
+    @Override
+    public void musicStarted(OlaScooter scooter) {
+        System.out.println("Transitioning to ParkedWithMusicState.");
         scooter.setState(ParkedWithMusicState.getInstance());
+    }
+
+    @Override
+    public void musicStopped(OlaScooter scooter) {
+        System.out.println("No music playing in ParkedState.");
     }
 }
 
+class StandbyState implements ScooterState {
+    private static final StandbyState instance = new StandbyState();
+
+    private StandbyState() {}
+
+    public static StandbyState getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void sideStandRemoved(OlaScooter scooter) {
+        System.out.println("No action taken, already in Standby Mode.");
+    }
+
+    @Override
+    public void sideStandParked(OlaScooter scooter) {
+        System.out.println("Transitioning to ParkedState.");
+        scooter.setState(ParkedState.getInstance());
+    }
+
+    @Override
+    public void powerButtonPressed(OlaScooter scooter) {
+        System.out.println("Scooter is already in Standby Mode. Ready to ride or park.");
+    }
+
+    @Override
+    public void musicStarted(OlaScooter scooter) {
+        System.out.println("Music cannot start in Standby Mode.");
+    }
+
+    @Override
+    public void musicStopped(OlaScooter scooter) {
+        System.out.println("Music is not playing in Standby Mode.");
+    }
+}
 
 class RideState implements ScooterState {
     private static final RideState instance = new RideState();
@@ -56,8 +102,19 @@ class RideState implements ScooterState {
     }
 
     @Override
-    public void startMusic(OlaScooter scooter) {
-        System.out.println("No action taken, music cannot play in Ride Mode.");
+    public void powerButtonPressed(OlaScooter scooter) {
+        System.out.println("Transitioning to StandbyState.");
+        scooter.setState(StandbyState.getInstance());
+    }
+
+    @Override
+    public void musicStarted(OlaScooter scooter) {
+        System.out.println("Music cannot start in RideState.");
+    }
+
+    @Override
+    public void musicStopped(OlaScooter scooter) {
+        System.out.println("Music cannot play in RideState.");
     }
 }
 
@@ -72,8 +129,8 @@ class ParkedWithMusicState implements ScooterState {
 
     @Override
     public void sideStandRemoved(OlaScooter scooter) {
-        System.out.println("HMI lock screen pops up. Ready to ride.");
-        scooter.setState(RideState.getInstance());
+        System.out.println("Press powerbutton to ride.");
+        scooter.setState(StandbyState.getInstance());
     }
 
     @Override
@@ -83,8 +140,19 @@ class ParkedWithMusicState implements ScooterState {
     }
 
     @Override
-    public void startMusic(OlaScooter scooter) {
-        System.out.println("Music is already playing.");
+    public void powerButtonPressed(OlaScooter scooter) {
+        System.out.println("Please remove the side stand to ride.");
+    }
+
+    @Override
+    public void musicStarted(OlaScooter scooter) {
+        System.out.println("Music is already playing in ParkedWithMusicState.");
+    }
+
+    @Override
+    public void musicStopped(OlaScooter scooter) {
+        System.out.println("Stopping music, transitioning to ParkedState.");
+        scooter.setState(ParkedState.getInstance());
     }
 }
 
@@ -112,7 +180,15 @@ class OlaScooter {
         state.sideStandParked(this);
     }
 
-    public void startMusic() {
-        state.startMusic(this);
+    public void powerButtonPressed() {
+        state.powerButtonPressed(this);
+    }
+
+    public void musicStarted() {
+        state.musicStarted(this);
+    }
+
+    public void musicStopped() {
+        state.musicStopped(this);
     }
 }
--- 680_HW4/src/test/umbcs680/statedesign/OlaScooterTest.java
+++ 680_HW4/src/test/umbcs680/statedesign/OlaScooterTest.java
@@ -6,10 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class OlaScooterTest {
 
     @Test
-    void ParkedStateToRideStateTransition() {
+    void ParkedStateToStandByStateTransition() {
         OlaScooter scooter = new OlaScooter(ParkedState.getInstance());
         scooter.sideStandRemoved();
-        assertTrue(scooter.getState() instanceof RideState, "Scooter should transition to Ride Mode.");
+        assertTrue(scooter.getState() instanceof StandbyState, "Scooter should transition to Standby Mode.");
     }
 
     @Test
@@ -34,16 +34,16 @@ class OlaScooterTest {
     }
 
     @Test
-    void MusicStartsInParkedState() {
+    void musicStartsInParkedState() {
         OlaScooter scooter = new OlaScooter(ParkedState.getInstance());
-        scooter.startMusic();
+        scooter.musicStarted();
         assertTrue(scooter.getState() instanceof ParkedWithMusicState, "Scooter should transition to Parked Mode with Music.");
     }
 
     @Test
-    void MusicDoesNotStartInRideState() {
+    void musicDoesNotStartInRideState() {
         OlaScooter scooter = new OlaScooter(RideState.getInstance());
-        scooter.startMusic();
+        scooter.musicStarted();
         assertFalse(scooter.getState() instanceof ParkedWithMusicState, "Scooter should not transition to Parked Mode with Music from Ride Mode.");
     }
 
@@ -55,23 +55,23 @@ class OlaScooterTest {
     }
 
     @Test
-    void TransitionFromMusicStateToRideState() {
+    void TransitionFromMusicStateToStandbyState() {
         OlaScooter scooter = new OlaScooter(ParkedWithMusicState.getInstance());
         scooter.sideStandRemoved();
-        assertTrue(scooter.getState() instanceof RideState, "Scooter should transition to Ride Mode when the side stand is removed.");
+        assertTrue(scooter.getState() instanceof StandbyState, "Scooter should transition to Ride Mode when the side stand is removed.");
     }
 
     @Test
-    void NoMusicTransitionWhenAlreadyPlaying() {
+    void noMusicTransitionWhenAlreadyPlaying() {
         OlaScooter scooter = new OlaScooter(ParkedWithMusicState.getInstance());
-        scooter.startMusic();
+        scooter.musicStarted();
         assertTrue(scooter.getState() instanceof ParkedWithMusicState, "Scooter should remain in Parked Mode with Music if music is already playing.");
     }
     
     @Test
-    void RideStateNoTransitionOnStartMusic() {
+    void rideStateNoTransitionOnMusicStart() {
         OlaScooter scooter = new OlaScooter(RideState.getInstance());
-        scooter.startMusic();
+        scooter.musicStarted();
         assertTrue(scooter.getState() instanceof RideState, "Scooter in Ride Mode should not transition when music starts.");
     }
 
@@ -80,5 +80,61 @@ class OlaScooterTest {
         OlaScooter scooter = new OlaScooter(ParkedState.getInstance());
         scooter.sideStandParked();
         assertTrue(scooter.getState() instanceof ParkedState, "Scooter in Parked State should not transition on side stand parked.");
+    }
+
+    @Test
+    void transitionToStandbyStateFromRideStateOnPowerPressed() {
+        OlaScooter scooter = new OlaScooter(RideState.getInstance());
+        scooter.powerButtonPressed();
+        assertTrue(scooter.getState() instanceof StandbyState, "Scooter should transition to Standby Mode when power button is pressed in Ride Mode.");
+    }
+
+    @Test
+    void standbyStateDoesNotChangeOnMusicStarted() {
+        OlaScooter scooter = new OlaScooter(StandbyState.getInstance());
+        scooter.musicStarted();
+        assertTrue(scooter.getState() instanceof StandbyState, "Scooter should remain in Standby Mode when music is started.");
+    }
+
+    @Test
+    void transitionToParkedStateFromStandbyStateOnSideStandParked() {
+        OlaScooter scooter = new OlaScooter(StandbyState.getInstance());
+        scooter.sideStandParked();
+        assertTrue(scooter.getState() instanceof ParkedState, "Scooter should transition to Parked Mode when side stand is parked in Standby Mode.");
+    }
+
+    @Test
+    void parkedStateToStandbyStateOnSideStandRemoved() {
+        OlaScooter scooter = new OlaScooter(ParkedState.getInstance());
+        scooter.sideStandRemoved();
+        assertTrue(scooter.getState() instanceof StandbyState, "Scooter should transition to Standby Mode when side stand is removed in Parked Mode.");
+    }
+
+    @Test
+    void transitionToRideStateFromStandbyStateOnSideStandRemoved() {
+        OlaScooter scooter = new OlaScooter(StandbyState.getInstance());
+        scooter.sideStandRemoved();
+        assertTrue(scooter.getState() instanceof StandbyState, "Scooter already in Standby Mode.");
+    }
+
+    @Test
+    void musicStoppedInParkedWithMusicStateTransitionsToParkedState() {
+        OlaScooter scooter = new OlaScooter(ParkedWithMusicState.getInstance());
+        scooter.musicStopped();
+        assertTrue(scooter.getState() instanceof ParkedState, "Scooter should transition to Parked State when music stops in ParkedWithMusic State.");
+    }
+
+    @Test
+    void musicDoesNotStartInStandbyState() {
+        OlaScooter scooter = new OlaScooter(StandbyState.getInstance());
+        scooter.musicStarted();
+        assertFalse(scooter.getState() instanceof ParkedWithMusicState, "Music should not start in Standby Mode, remaining in Standby State.");
+    }
+
+    @Test
+    void musicDoesNotStopInStandbyState() {
+        OlaScooter scooter = new OlaScooter(StandbyState.getInstance());
+        scooter.musicStopped();
+        assertTrue(scooter.getState() instanceof StandbyState, "Music stop action should not change Standby State.");
     }
 }
