12352d443b7cef33de36e00c18b45daca626f954
chandu37421104 <144286107+chandu37421104@users.noreply.github.com>
2024-05-11 00:00:35 -0400

    Delete 680_HW15/src directory

--- 680_HW15/src/main/umbcs680/color/ColorAdjuster.java
+++
@@ -1,5 +0,0 @@
-package umbcs680.color;
-
-public interface ColorAdjuster {
-    Color adjust(Color color);
-}
--- 680_HW15/src/main/umbcs680/color/GrayScaleAdjuster.java
+++
@@ -1,9 +0,0 @@
-package umbcs680.color;
-
-public class GrayScaleAdjuster implements ColorAdjuster {
-    @Override
-    public Color adjust(Color color) {
-        int avg = (color.getRedScale() + color.getGreenScale() + color.getBlueScale()) / 3;
-        return new Color(avg, avg, avg);
-    }
-}
